a09459388c9766764bba24044c8af0bf32244f6d
noredine ban <lhrinourdine9@gmail.com>
2024-08-29 00:56:50 +0100

    test

--- wandercult-main/server.js
+++ wandercult-main/server.js
@@ -587,7 +587,7 @@ app.get("/api/reports", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
